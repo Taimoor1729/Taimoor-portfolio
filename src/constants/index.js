767d90aa164e2dcebbf03915e4501f7b099e0ebb
taimoor1729 <temitech669@gmail.com>
2024-03-14 15:02:10 +0500

    taimoor portfolio is ready

---
+++ src/constants/index.js
@@ -0,0 +1,529 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    typescript,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    docker,
+    carrent,
+    jobit,
+    tripguide,
+    cloudtek,
+    aksaSds,
+    deltaShoppe,
+    java,
+    angular,
+    laravel,
+    nextjs,
+    postgresql,
+    git,
+    mysimplerx,
+    vars,
+    flexigolf, chotok, portfolio, ugap, edfry,python,TaskManagement
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Full-Stack Developer",
+        icon: web,
+    },
+    {
+        title: "Mobile App Developer",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "Front-end Developer",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "NextJs",
+        icon: nextjs,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Angular",
+        icon: python,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    // {
+    //     name: 'Java',
+    //     icon: java,
+    // },
+    // {
+    //     name: "Laravel",
+    //     icon: laravel,
+    // },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "PostgreSQL",
+        icon: postgresql,
+    },
+    {
+        name: "docker",
+        icon: docker,
+    },
+    {
+        name: 'git',
+        icon: git
+    }
+];
+
+const experiences = [
+    {
+        title: "React JS developer",
+        company_name: "CyberSoft Vantage",
+        icon: cloudtek,
+        iconBg: "#fff",
+        date: "Aug 2022 - Present",
+        points: [
+            "Developed interactive and responsive user interfaces using React.js, enabling seamless and engaging web experiences.",
+            "Collaborated closely with designers, product managers, and fellow developers to transform design concepts into functional and visually appealing web applications.",
+            "Played an active role in code reviews, ensuring code quality, adherence to best practices, and fostering a culture of collaboration and continuous improvement.",
+            "Demonstrated proficiency in state management and component lifecycle methods, optimizing the performance and efficiency of React.js applications.",
+            "Implemented complex user interface components, leveraging React.js libraries and custom solutions to meet project requirements.",
+            "Utilized routing libraries such as React Router to create single-page applications with efficient navigation and URL handling.",
+            "Integrated with RESTful APIs and GraphQL endpoints to fetch and display data dynamically, enhancing the interactivity of web applications.",
+            "Contributed to the optimization and refactoring of existing codebases", 
+        ]
+    },
+    {
+        title: "React Native developer",
+        company_name: "CyberSoft Vantage",
+        icon: cloudtek,
+        iconBg: "#E6DEDD",
+        date: "Dec 2021 - Aug 2022",
+        points: [
+            "Contributed to the development of cross-platform mobile applications using React Native, allowing for efficient code sharing across iOS and Android platforms.",
+            "Worked closely with a multidisciplinary team, including designers and backend developers, to deliver mobile applications that meet project requirements and deadlines.",
+            "Participated in the design and implementation of user interfaces, ensuring a visually appealing and intuitive user experience for app users.",
+            "Collaborated in code reviews to maintain code quality, identify potential issues, and implement improvements to the codebase.",
+            "Gained proficiency in React Native components, navigation, and state management, enhancing the overall functionality and interactivity of mobile apps.",
+            "Adapted to changing project requirements and priorities, demonstrating flexibility and problem-solving skills in a dynamic development environment.",
+            "Integrated third-party libraries and APIs to extend the capabilities of mobile applications, such as adding geolocation services, push notifications, or social media sharing.",
+            "Implemented responsive design principles to optimize mobile apps for various screen sizes and resolutions, ensuring a consistent user experience.",
+            "Demonstrated a commitment to learning and staying up-to-date with React Native and mobile development trends, applying newfound knowledge to improve project outcomes.",
+            "Contributed to bug fixes and performance optimizations, addressing issues promptly to maintain a smooth and efficient mobile app experience.",
+            "Participated in the creation and maintenance of project documentation, including technical specifications and user guides, to support ongoing development and collaboration.",
+            "Collaborated with the QA team to identify and resolve issues, ensuring the delivery of high-quality, bug-free mobile applications.",
+            "Effectively communicated project progress, challenges, and solutions to team members and project stakeholders, fostering transparency and alignment.",
+            "Despite limited project duration, made valuable contributions to the development of mobile applications, showcasing adaptability and rapid skill acquisition."
+        ]
+    },
+    {
+        title: "Jr. MERN Developer",
+        company_name: "CyberSoft Vantage",
+        icon: cloudtek,
+        iconBg: "#fff",
+        date: "Mar 2021 - Nov 2021",
+        points: [
+            "Developed and maintained web applications using the MERN (MongoDB, Express.js, React, Node.js) stack, focusing on creating robust and scalable solutions.",
+            "Collaborated closely with cross-functional teams, including designers, project managers, and fellow developers, to deliver high-quality web products within project timelines.",
+            "Participated actively in code reviews, ensuring code quality, adherence to best practices, and fostering a culture of collaboration and improvement.",
+            "Proficiently used Git and GitHub for version control, enabling efficient collaboration with team members and maintaining code consistency throughout the development process.",
+            "Designed and implemented responsive and mobile-friendly web applications, guaranteeing a seamless user experience across various devices and screen sizes.",
+            "Successfully integrated third-party APIs and libraries to enhance the functionality and features of web applications, adding value and versatility to projects.",
+            "Maintained comprehensive documentation of software design and codebase, ensuring clear communication, efficient code maintenance, and scalability for future development.",
+            "Optimized database systems, including MongoDB, to improve query performance and enhance the efficiency of data retrieval and storage.",
+            "Contributed to the development of financial web applications, including projects such as VARS and MySimpleRx, demonstrating expertise in handling complex data and transactions."
+        ]
+        
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "Taimoor is a web-app wizard, he made sure to deliver on time. Will be looking forward to hiring him again. Impressed with is grip on the technicalities involved in the dev-stack he is working in.",
+        name: "Areeb Ali",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    {
+        testimonial:
+            "Proactive and Professional way of working, Good seller",
+        name: "DeHypto",
+        designation: "Client",
+        company: "Fiverp",
+        image: "https://randomuser.me/api/portraits/men/2.jpg",
+    },
+    {
+        testimonial:
+            "Very fast and excellent delivery, enjoyed working with him, It's a pleasure working with him highly recommended.",
+        name: "SleeHom",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/3.jpg",
+    },
+    {
+        testimonial:
+            "Very perfect, made the right choice working with him, Thanks for the Good work, Highly recommended",
+        name: "Lerthert",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+    {
+        testimonial:
+            "work was well done",
+        name: "bless7",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+        testimonial:
+            "good job. proper work.",
+        name: "tjdavis",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/6.jpg",
+    },
+    {
+        testimonial:
+            "Amazing delivery and before time ! Kind, friendly and helpful. Would order again. Thank you :)",
+        name: "secretdoctor",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/7.jpg",
+    },
+    {
+        testimonial:
+            "amazing work, give a try to this seller",
+        name: "mrhazwansalam",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/8.jpg",
+    },
+    {
+        testimonial:
+            "make my 3rd order with him, he is so friendly..... mostly recommended",
+        name: "mrhazwansalam",
+        designation: "Client",
+        company: "Fiver",
+        image: "https://randomuser.me/api/portraits/men/8.jpg",
+    },
+   
+];
+
+const projects = [
+    {
+        name: "Portfolio",
+        description:
+            "M.Taimoor is a personal website showcasing my portfolio and projects. It offers a glimpse into my skills, experience, and creative work, providing an opportunity to explore  talents and accomplishments.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "threejs",
+                color: "red-text-gradient",
+            },
+            {
+                name: "vitejs",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwindcss",
+                color: "pink-text-gradient",
+            },
+            // {
+            //     name: "java",
+            //     color: "pink-text-gradient",
+            // },
+            // {
+            //     name: "springBoot",
+            //     color: "red-text-gradient",
+            // },
+            {
+                name: "microservices",
+                color: "blue-text-gradient",
+            },
+        ],
+
+        image: portfolio,
+        source_code_link: "https://behlole.github.io",
+
+    },
+    // {
+    //     name: "MySimpleRx",
+    //     description:
+    //         "MySimplerx.com is a user-friendly website that simplifies medication management. It offers secure prescription storage, medication tracking, and refill requests, ensuring easy organization and adherence to treatment plans.",
+    //     tags: [
+    //         {
+    //             name: "angular",
+    //             color: "blue-text-gradient",
+    //         },
+    //         {
+    //             name: "laravel",
+    //             color: "green-text-gradient",
+    //         },
+    //         {
+    //             name: "postgresql",
+    //             color: "pink-text-gradient",
+    //         },
+    //         {
+    //             name: "laravelLumen",
+    //             color: "red-text-gradient",
+    //         },
+    //         {
+    //             name: "microservices",
+    //             color: "blue-text-gradient",
+    //         },
+    //     ],
+    //     image: mysimplerx,
+    //     source_code_link: "https://mysimplerx.com",
+    // },
+    {
+        name: "IBDAA",
+        description:
+            "it is a single page web app ",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            // {
+            //     name: "restapi",
+            //     color: "green-text-gradient",
+            // },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            // {
+            //     name: "angular",
+            //     color: "blue-text-gradient",
+            // },
+            // {
+            //     name: "laravel",
+            //     color: "green-text-gradient",
+            // },
+            {
+                name: "Styled component",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "Font Awesome",
+                color: "red-text-gradient",
+            },
+            {
+                name: "Single Page",
+                color: "blue-text-gradient",
+            },
+        ],
+        image: vars,
+        source_code_link: "https://theibdaa.com/",
+    },
+    {
+        name: "FlexiGolf",
+        description:
+            "website that offers a flexible and convenient way to book and manage golf tee times in Australia. With a user-friendly interface, golf enthusiasts can easily search for available tee times at various courses, book their preferred slots, and even invite friends to join their game. FlexiGolf.com.au aims to streamline the golf booking process, providing golfers with a hassle-free experience and ensuring they make the most of their time on the greens.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "angular",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "laravel",
+                color: "green-text-gradient",
+            },
+            {
+                name: "postgresql",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "laravelLumen",
+                color: "red-text-gradient",
+            },
+            {
+                name: "microservices",
+                color: "blue-text-gradient",
+            },
+        ],
+
+        image: flexigolf,
+        source_code_link: "https://flexigolf.com.au",
+    },
+    {
+        name: "Task Management",
+        description:
+            "Task Management app is just like jira borad but some extra feature. With a wide range of options for various events and activities, users can easily find and enhance the board and tickets for their preferred entertainment and leisure experiences.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            // {
+            //     name: "angular",
+            //     color: "blue-text-gradient",
+            // },
+            {
+                name: "redux",
+                color: "green-text-gradient",
+            },
+            {
+                name: "javaScript",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "redux toolkit",
+                color: "red-text-gradient",
+            },
+            // {
+            //     name: "microservices",
+            //     color: "blue-text-gradient",
+            // },
+        ],
+
+        image: TaskManagement,
+        source_code_link: "http://taimoor1729.github.io/Task-Management",
+    },
+    // {
+    //     name: "UGAP",
+    //     description:
+    //         "ugap.net is a website that serves as a platform for the UGAP (Union des Groupements d'Achats Publics) organization. It offers comprehensive information and resources related to public procurement, providing government entities and public institutions with access to procurement solutions, contracts, and procurement advice.",
+    //     tags: [
+    //         {
+    //             name: "laravel",
+    //             color: "blue-text-gradient",
+    //         },
+    //         {
+    //             name: "restapi",
+    //             color: "green-text-gradient",
+    //         },
+    //         {
+    //             name: "scss",
+    //             color: "pink-text-gradient",
+    //         },
+    //         {
+    //             name: "html",
+    //             color: "blue-text-gradient",
+    //         },
+    //         {
+    //             name: "laravel",
+    //             color: "green-text-gradient",
+    //         },
+    //         {
+    //             name: "mysql",
+    //             color: "pink-text-gradient",
+    //         },
+    //     ],
+
+    //     image: ugap,
+    //     source_code_link: "https://ugap.net",
+    // },
+    {
+        name: "EdFry",
+        description:
+            "edfry.co is a website that provides a platform for booking mentorship sessions with Ed Fry. As a seasoned mentor, Ed offers guidance and expertise in various areas such as software development, design, and professional growth. Visitors can schedule sessions with Ed to gain valuable insights and advice to advance their careers.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "scss",
+                color: "pink-text-gradient",
+            },
+            // {
+            //     name: "angular",
+            //     color: "blue-text-gradient",
+            // },
+            // {
+            //     name: "laravel",
+            //     color: "green-text-gradient",
+            // },
+            {
+                name: "postgresql",
+                color: "pink-text-gradient",
+            },
+            // {
+            //     name: "laravelLumen",
+            //     color: "red-text-gradient",
+            // },
+            {
+                name: "microservices",
+                color: "blue-text-gradient",
+            },
+        ],
+        image: edfry,
+        source_code_link: "https://edfry.co",
+    },
+
+];
+
+export {services, technologies, experiences, testimonials, projects};
